d74b75bc5ac062765295eb5fbc8671d56452f74f
VijayKadiyam <freelancervijayk@gmail.com>
2019-07-19 00:04:48 +0530

    First commit

--- app/Http/Controllers/TimesController.php
+++ app/Http/Controllers/TimesController.php
@@ -8,12 +8,14 @@ class TimesController extends Controller
 {
   public function index()
   {
-    $endpoint = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
+    // $endpoint = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
+    $endpoint = "http://worldclockapi.com/api/json/est/now";
     $client = new \GuzzleHttp\Client();
 
     $response = $client->request('GET', $endpoint);
     $statusCode = $response->getStatusCode();
     $content = json_decode($response->getBody(), true);
+    $content['datetime'] = $content['currentDateTime'];
 
     return response()->json([
       'data'  =>  $content
