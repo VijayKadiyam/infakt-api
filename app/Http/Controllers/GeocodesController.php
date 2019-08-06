f1af77404ed35f0dd20a5679423db2206f663661
VijayKadiyam <freelancervijayk@gmail.com>
2019-08-06 10:29:34 +0530

    Geocode added

--- app/Http/Controllers/GeocodesController.php
+++ app/Http/Controllers/GeocodesController.php
@@ -6,16 +6,14 @@ use Illuminate\Http\Request;
 
 class GeocodesController extends Controller
 {
-  public function index()
+  public function index(Request $request)
   {
-    $endpoint = "https://maps.googleapis.com/maps/api/geocode/json?latlng=18.9928863,72.8447738&key=AIzaSyDSECwAUD8Ynppe3u_MGuczSeDsH7uP2FQ";
+    $endpoint = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" . $request->lat . "," . $request->lng . "&key=AIzaSyDSECwAUD8Ynppe3u_MGuczSeDsH7uP2FQ";
     $client = new \GuzzleHttp\Client();
 
     $response = $client->request('GET', $endpoint);
     $statusCode = $response->getStatusCode();
     $content = json_decode($response->getBody(), true);
-
-    dd($content);
 
     return response()->json([
       'data'  =>  $content
