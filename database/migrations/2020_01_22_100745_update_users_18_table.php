56171fbb87f2b062ccd2639850fcf1b85860dbf0
VijayKadiyam <freelancervijayk@gmail.com>
2020-01-22 10:50:46 +0530

    checked added

--- database/migrations/2020_01_22_100745_update_users_18_table.php
+++ database/migrations/2020_01_22_100745_update_users_18_table.php
@@ -14,10 +14,10 @@ class UpdateUsers18Table extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('pds_form_checked')->default(false)->change();
-            $table->boolean('form_2_checked')->default(false)->change();
-            $table->boolean('form_11_checked')->default(false)->change();
-            $table->boolean('graduity_form_checked')->default(false)->change();
+            $table->boolean('pds_form_checked')->default(false);
+            $table->boolean('form_2_checked')->default(false);
+            $table->boolean('form_11_checked')->default(false);
+            $table->boolean('graduity_form_checked')->default(false);
         });
     }
 
