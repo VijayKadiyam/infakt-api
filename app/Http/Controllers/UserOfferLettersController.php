c8504596e90c6d4b22340aca3b6f222271e6a79b
VijayKadiyam <freelancervijayk@gmail.com>
2020-03-21 09:29:08 +0530

    Offer letter added

--- app/Http/Controllers/UserOfferLettersController.php
+++ app/Http/Controllers/UserOfferLettersController.php
@@ -64,7 +64,7 @@ class UserOfferLettersController extends Controller
     $data['user'] = $user;
     $data['letter'] = $userOfferLetter;
 
-    $pdf = PDF::loadView('letters.ol', $data);
+    return $pdf = PDF::loadView('letters.ol', $data);
 
     // return $pdf->download($user->name . '-offer-letter.pdf');
   }
