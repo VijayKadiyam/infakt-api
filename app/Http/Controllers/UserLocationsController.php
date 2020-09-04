90a02bcf10c60be47071be2fa3a9c015cf6faca3
VijayKadiyam <freelancervijayk@gmail.com>
2020-09-04 12:51:40 +0530

    Checking location

--- app/Http/Controllers/UserLocationsController.php
+++ app/Http/Controllers/UserLocationsController.php
@@ -73,8 +73,8 @@ class UserLocationsController extends Controller
     
     if(sizeof($request->user()->supervisors) > 0) {
       $checkLocations = UserLocation::whereDate('created_at', '=', Carbon::parse($userLocation->created_at)->format('Y-m-d'))->get();
-      // if(sizeof($checkLocations) == 1 && $request->user()->id == 375) {
-      if($request->user()->id == 375) {
+      if(sizeof($checkLocations) == 1) {
+      // if($request->user()->id == 375) {
         $address = json_decode($geocodesController->index($request)->getContent())->data;
         $userLocation->address = $address;
         $userLocation->update();
