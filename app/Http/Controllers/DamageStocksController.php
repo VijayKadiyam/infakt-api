1fd34d1a96ff392e380edc57e57c1e96728f7488
aabidSh <aabid@aaibuzz.com>
2021-02-09 15:33:52 +0530

    Search funcnatlity in damage stock Api

--- app/Http/Controllers/DamageStocksController.php
+++ app/Http/Controllers/DamageStocksController.php
@@ -11,106 +11,62 @@ class DamageStocksController extends Controller
     {
       $this->middleware(['auth:api', 'company']);
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $damageStock = request()->company->damage_stocks;
 
+    public function index(Request $request)
+    {
+      if($request->search) {
+      $damageStock = request()->company->damage_stocks()->where('created_at', 'LIKE', '%' . $request->search . '%')
+        ->get();
+      }
+      else{
+        $damageStock = request()->company->damage_stocks;
+      }
+
+      return response()->json([
+      'data'     => $damageStock,
+      'success'  => true
+      ], 200);
+    }
+
+    public function store(Request $request)
+    {
+      $request->validate([
+          'qty'    =>  'required',
+
+        ]);
+    
+        $damageStock = new DamageStock($request->all());
+        $request->company->damage_stocks()->save($damageStock);
+    
         return response()->json([
-        'data'     => $damageStock,
-        'success'  => true
+          'data'    =>  $damageStock,
+          'success' =>  true
+        ], 201); 
+    }
+
+    public function show(DamageStock $damageStock)
+    {
+      return response()->json([
+          'data'   =>  $damageStock
         ], 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'qty'    =>  'required',
-
-          ]);
-      
-          $damageStock = new DamageStock($request->all());
-          $request->company->damage_stocks()->save($damageStock);
-      
-          return response()->json([
-            'data'    =>  $damageStock,
-            'success' =>  true
-          ], 201); 
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(DamageStock $damageStock)
-    {
-        return response()->json([
-            'data'   =>  $damageStock
-          ], 200);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, DamageStock $damageStock)
     {
-        $request->validate([
-            'qty'  =>  'required',
-          ]);
-      
-          $damageStock->update($request->all());
-          
-          return response()->json([
-            'data'  =>  $damageStock,
-            'success' =>  true
-          ], 200);
+      $request->validate([
+          'qty'  =>  'required',
+        ]);
+    
+        $damageStock->update($request->all());
+        
+        return response()->json([
+          'data'  =>  $damageStock,
+          'success' =>  true
+        ], 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(DamageStock $damageStock)
     {
-        $damageStock->delete();
+      $damageStock->delete();
     }
 }
