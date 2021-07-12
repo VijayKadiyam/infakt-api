860249125ac855c061263c56eaf0a8d724e1e6eb
VijayKadiyam <freelancervijayk@gmail.com>
2021-07-12 13:14:40 +0530

    UserAttendanceController 1.1

--- app/Http/Controllers/UserAttendancesController.php
+++ app/Http/Controllers/UserAttendancesController.php
@@ -106,6 +106,25 @@ class UserAttendancesController extends Controller
 
     $user = User::find($userAttendance->user_id);
     $geocodesController = new GeocodesController();
+    if($user->so != null) {
+      if($userAttendance->login_time && $userAttendance->login_lat)
+      {
+        $request->request->add(['lat' => $userAttendance->login_lat]);
+        $request->request->add(['lng' => $userAttendance->login_lng]);
+
+        $address = json_decode($geocodesController->index($request)->getContent())->data;
+        $phone = $user->so->phone;
+        $name = $user->name;
+        $date = $userAttendance->date;
+        $time = $userAttendance->login_time;
+        $lat = $userAttendance->login_lat;
+        $lng = $userAttendance->login_lng;
+        $battery = '-';
+        $address = $address;
+        
+        $this->sendSMS($phone, $name, $date, $time, $lat, $lng, $battery, $address);
+      }
+    }
     if(sizeof($user->supervisors) > 0)
       if($userAttendance->login_time && $userAttendance->login_lat)
       {
