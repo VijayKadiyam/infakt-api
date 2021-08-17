9126c8b2cae3a9cfb4f06974182e519d3cf5cae0
VijayKadiyam <freelancervijayk@gmail.com>
2021-08-17 18:41:37 +0530

    LC

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -220,7 +220,8 @@ class UsersController extends Controller
   {
     $request->validate([
       'name'                    => ['required', 'string', 'max:255'],
-      'email'                   => ['required', 'string', 'email', 'max:255'],
+      // 'email'                   => ['required', 'string', 'email', 'max:255'],
+      'email'                   => ['required', 'string', 'max:255'],
       'phone'                   =>  'required',
       // 'doj'                     =>  'required',
       // 'dob'                     =>  'required',
