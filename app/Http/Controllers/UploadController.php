5d2fdff124e935f90d74cc3e5448121b52931a33
Khan-Sohail <ksohail.sk32@gmail.com>
2021-09-22 16:32:27 +0530

    Upload Image Path in Daily photos Done

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\DailyPhoto;
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Storage;
@@ -123,5 +124,85 @@ class UploadController extends Controller
       ],
       'success' =>  true
     ]);
+  }
+
+  public function uploadDailyPhotos(Request $request)
+  {
+    $request->validate([
+      'image_path'        => 'required',
+    ]);
+
+    $imagePath = '';
+    if ($request->hasFile('image_path')) {
+      $file = $request->file('image_path');
+      $name = $request->filename ?? 'photo.';
+      $name = $name . $file->getClientOriginalExtension();;
+      $imagePath = 'dailyphotos/' .  $request->id . '/' . $name;
+      Storage::disk('local')->put($imagePath, file_get_contents($file), 'public');
+
+      $dailyphoto = DailyPhoto::where('id', '=', request()->id)->first();
+      $dailyphoto->image_path = $imagePath;
+      $dailyphoto->update();
+    }
+
+    $imagePath1='';
+    if ($request->hasFile('image_path1')) {
+      $file = $request->file('image_path1');
+      $name = $request->filename ?? 'photo.';
+      $name = $name . $file->getClientOriginalExtension();;
+      $imagePath1 = 'dailyphotos/' .  $request->id . '/' . $name;
+      Storage::disk('local')->put($imagePath1, file_get_contents($file), 'public');
+
+      $dailyphoto = DailyPhoto::where('id', '=', request()->id)->first();
+      $dailyphoto->image_path1 = $imagePath1;
+      $dailyphoto->update();
+    }
+    $imagePath2='';
+    if ($request->hasFile('image_path2')) {
+      $file = $request->file('image_path2');
+      $name = $request->filename ?? 'photo.';
+      $name = $name . $file->getClientOriginalExtension();;
+      $imagePath2 = 'dailyphotos/' .  $request->id . '/' . $name;
+      Storage::disk('local')->put($imagePath2, file_get_contents($file), 'public');
+
+      $dailyphoto = DailyPhoto::where('id', '=', request()->id)->first();
+      $dailyphoto->image_path2 = $imagePath2;
+      $dailyphoto->update();
+    }
+    $imagePath3='';
+    if ($request->hasFile('image_path3')) {
+      $file = $request->file('image_path3');
+      $name = $request->filename ?? 'photo.';
+      $name = $name . $file->getClientOriginalExtension();;
+      $imagePath3 = 'dailyphotos/' .  $request->id . '/' . $name;
+      Storage::disk('local')->put($imagePath3, file_get_contents($file), 'public');
+
+      $dailyphoto = DailyPhoto::where('id', '=', request()->id)->first();
+      $dailyphoto->image_path3 = $imagePath3;
+      $dailyphoto->update();
+    }
+    $imagePath4='';
+    if ($request->hasFile('image_path4')) {
+      $file = $request->file('image_path4');
+      $name = $request->filename ?? 'photo.';
+      $name = $name . $file->getClientOriginalExtension();;
+      $imagePath4 = 'dailyphotos/' .  $request->id . '/' . $name;
+      Storage::disk('local')->put($imagePath4, file_get_contents($file), 'public');
+
+      $dailyphoto = DailyPhoto::where('id', '=', request()->id)->first();
+      $dailyphoto->image_path4 = $imagePath4;
+      $dailyphoto->update();
+    }
+    return response()->json([
+      'data'  => [
+        'image_path'  =>  $imagePath,
+        'image_path1' => $imagePath1,
+        'image_path2' => $imagePath2,
+        'image_path3' => $imagePath3,
+        'image_path4' => $imagePath4,
+      ],
+      'success' =>  true
+    ]);
+  
   }
 }
