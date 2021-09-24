dcb9dd5385a741dcdab31ce2ce2ee00f9cb1ea1a
Khan-Sohail <ksohail.sk32@gmail.com>
2021-09-24 11:54:23 +0530

    Tracker & Tracker Sku  Module Added

---
+++ database/migrations/2021_09_24_112843_create_trackers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrackersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trackers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id');
+            $table->integer('retailer_id')->nullable();
+            $table->string('customer_name',100)->nullable();
+            $table->string('contact_no',100)->nullable();
+            $table->string('email_id',100)->nullable();
+            $table->string('tracker_type',100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trackers');
+    }
+}
