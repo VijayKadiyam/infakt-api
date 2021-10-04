4492b12d0632f1c5914e8da28befe9c9099e9143
aabidSh <aabid@aaibuzz.com>
2021-10-04 18:16:13 +0530

    Pjp Markets Done Changes

--- app/Company.php
+++ app/Company.php
@@ -428,7 +428,8 @@ class Company extends Model
   }
   public function pjps()
   {
-    return $this->hasMany(Pjp::class);
+    return $this->hasMany(Pjp::class)
+    ->with('pjp_markets');
   }
   public function pjp_markets()
   {
