1b48ae1a490249ff1818d6a6b24a14d62b6f3bfc
Shaikh-Aabid <shaikhaabidhussain@gmail.com>
2021-10-30 13:39:15 +0530

    Update Target Import File

--- app/Imports/TargetImport.php
+++ app/Imports/TargetImport.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Imports;
+
 use App\CrudeTarget;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -10,9 +11,9 @@ HeadingRowFormatter::default('none');
 
 class TargetImport implements ToModel, WithHeadingRow
 {
-    public function model(array $row)
-    {
-
+  public function model(array $row)
+  {
+    if ($row['Target'] != '') {
       $data = [
         'company_id'  =>  request()->company->id,
         'store_code' =>  $row['Store Code'],
@@ -20,17 +21,19 @@ class TargetImport implements ToModel, WithHeadingRow
         'month'  =>  request()->month,
         'year'  =>  request()->year,
       ];
-  
       return new CrudeTarget($data);
     }
-  
-    public function headingRow(): int
-    {
-      return 1;
-    }
-  
-    public function batchSize(): int
-    {
-      return 1000;
-    }
+
+
   }
+
+  public function headingRow(): int
+  {
+    return 1;
+  }
+
+  public function batchSize(): int
+  {
+    return 1000;
+  }
+}
