6e27330817f87e4deb44a03cfdd119420a9bc455
Shaikh-Aabid <shaikhaabidhussain@gmail.com>
2021-11-20 16:16:11 +0530

    CR: Change crude users as per arvind sheet

--- app/Imports/UserMappingImport.php
+++ app/Imports/UserMappingImport.php
@@ -24,7 +24,7 @@ class UserMappingImport implements ToModel, WithHeadingRow
                 'billing_code'       =>  (array_key_exists('Billing Code', $row) && $row['Billing Code']) ? $row['Billing Code'] : null,
                 'store_code'       =>  $row['New Store CD'],
                 'store_name'        =>  $row['Store Name'],
-                // 'store_address'       =>  $row['Store Address'],
+                'store_address'       =>  $row['Store Address'],
                 'ba_name'       =>  $row['BA Name'],
                 'location'        =>  $row['Location'],
                 'city'       =>  $row['City'],
@@ -34,6 +34,7 @@ class UserMappingImport implements ToModel, WithHeadingRow
                 'supervisor_name'       =>  $row['Supervisor Name'],
                 'brand'       => (array_key_exists('BRAND', $row) && $row['BRAND']) ? $row['BRAND'] : NULL,
                 'ba_status'       => (array_key_exists('BA status', $row) && $row['BA status'])  ? $row['BA status'] : NULL,
+                'doj'       => (array_key_exists('DOJ', $row) && $row['DOJ'])  ? $row['DOJ'] : NULL,
 
                 // 'store_type'        => (array_key_exists('Store Type', $row) && $row['Store Type'])  ? $row['Store Type'] : NULL,
                 // 'store_status'       => (array_key_exists('Store Status', $row) && $row['Store Status'])  ? $row['Store Status'] : NULL,
