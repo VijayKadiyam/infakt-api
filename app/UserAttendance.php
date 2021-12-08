baee637d295aa78eef67c962d962c992c1252cf2
Shaikh-Aabid <shaikhaabidhussain@gmail.com>
2021-12-08 18:48:30 +0530

    Working time display and designation

--- app/UserAttendance.php
+++ app/UserAttendance.php
@@ -17,7 +17,7 @@ class UserAttendance extends Model
    */
   public function user()
   {
-    return $this->belongsTo(User::class);
+    return $this->belongsTo(User::class)->with('roles');
   }
 
   /*
