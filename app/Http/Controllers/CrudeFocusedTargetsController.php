466f43198432a5293f28a6f2c4cbb18d060c6549
Shaikh-Aabid <shaikhaabidhussain@gmail.com>
2022-01-11 17:30:21 +0530

    Debugging

--- app/Http/Controllers/CrudeFocusedTargetsController.php
+++ app/Http/Controllers/CrudeFocusedTargetsController.php
@@ -44,96 +44,99 @@ class CrudeFocusedTargetsController extends Controller
 
         $crude_focused_targets = CrudeFocusedTarget::all();
         $category = [
-            'baby', // category
-            'baby_Kit',
-            'baby_Oil',
-            'bath_salt',
-            'bathing',
-            'body',
-            'body_butter',
-            'body_cream',
+            // 'baby', // category
+            // 'baby_Kit',
+            // 'baby_Oil',
+            // 'bath_salt',
+            // 'bathing',
+            // 'body',
+            // 'body_butter',
+            // 'body_cream',
             'body_lotion',
-            'body_scrub',
-            'body_wash',
-            'capsules',
-            'cleanser',
-            'cleansing',
-            'combo_kit',
-            'conditioner',
-            'cream',
-            'diaper',
-            'dusting_powder',
-            'face_cream',
-            'face_free',
-            'face_Mask',
-            'face_Milk',
-            'face_scrub',
-            'face_Serum',
-            'face_Spot',
-            'face_toner',
-            'facewash',
-            'freebies',
-            'gel',
-            'gift_pack',
-            'hair_care',
-            'hair_Mask',
-            'hair_Oil',
-            'hair_Serum',
-            'hand_cream',
-            'hygine',
-            'kajal',
-            'kids_body',
-            'lip_balm',
-            'lotion',
-            'mask',
-            'moisturizer',
-            'mosquito_protection',
-            'oil',
-            'oral',
-            'otc',
-            'peeling',
-            'serum',
-            'shampoo',
-            'sheet_mask',
-            'sub_cat',
-            'sun_cat',
-            'sun_care',
-            'sunscreen',
-            'tablets',
-            'toner',
-            'yogurt_for',
+            // 'body_scrub',
+            // 'body_wash',
+            // 'capsules',
+            // 'cleanser',
+            // 'cleansing',
+            // 'combo_kit',
+            // 'conditioner',
+            // 'cream',
+            // 'diaper',
+            // 'dusting_powder',
+            // 'face_cream',
+            // 'face_free',
+            // 'face_Mask',
+            // 'face_Milk',
+            // 'face_scrub',
+            // 'face_Serum',
+            // 'face_Spot',
+            // 'face_toner',
+            // 'facewash',
+            // 'freebies',
+            // 'gel',
+            // 'gift_pack',
+            // 'hair_care',
+            // 'hair_Mask',
+            // 'hair_Oil',
+            // 'hair_Serum',
+            // 'hand_cream',
+            // 'hygine',
+            // 'kajal',
+            // 'kids_body',
+            // 'lip_balm',
+            // 'lotion',
+            // 'mask',
+            // 'moisturizer',
+            // 'mosquito_protection',
+            // 'oil',
+            // 'oral',
+            // 'otc',
+            // 'peeling',
+            // 'serum',
+            // 'shampoo',
+            // 'sheet_mask',
+            // 'sub_cat',
+            // 'sun_cat',
+            // 'sun_care',
+            // 'sunscreen',
+            // 'tablets',
+            // 'toner',
+            // 'yogurt_for',
         ];
         $focus_target = [];
         foreach ($crude_focused_targets as $column =>  $target) {
             if ($target->store_code) {
                 $us = User::where('employee_code', '=', $target->store_code)
                     ->first();
-                $user_id = $us['id'];
+                if ($us) {
 
-                $data = [
-                    'company_id' => request()->company->id,
-                    'user_id' => $user_id,
-                    'month' => $target->month,
-                    'year' => $target->year,
-                ];
+                    $user_id = $us['id'];
 
-                foreach ($category as $key => $cat) {
-                    if ($target->$cat) {
-                        $category_target = $target[$cat];
-                        $data['target'] = $category_target;
-                        $data['category'] = $cat;
-                        $User_target = FocusedTarget::where('user_id', '=', $user_id)
-                            ->where('month', '=', $target->month)
-                            ->where('year', '=', $target->year)
-                            ->where('category', '=', $cat)->first();
-                        if ($User_target) {
-                            // Update FocusedTarget
-                            $targetData = FocusedTarget::where('id', '=', $User_target->id);
-                            $targetData->update($data);
-                        } else {
-                            // Insert FocusedTarget
-                            $targetData = new FocusedTarget($data);
-                            $targetData->save();
+                    $data = [
+                        'company_id' => request()->company->id,
+                        'user_id' => $user_id,
+                        'month' => $target->month,
+                        'year' => $target->year,
+                    ];
+
+                    foreach ($category as $key => $cat) {
+                        if ($target->$cat) {
+                            $category_target = $target[$cat];
+                            $data['target'] = $category_target;
+                            $data['category'] = $cat;
+                            $User_target = FocusedTarget::where('user_id', '=', $user_id)
+                                ->where('month', '=', $target->month)
+                                ->where('year', '=', $target->year)
+                                ->where('category', '=', $cat)->first();
+                            if ($User_target) {
+                                // Update FocusedTarget
+                                $targetData = FocusedTarget::where('id', '=', $User_target->id);
+                                $targetData->update($data);
+                            } else {
+                                // Insert FocusedTarget
+                                $targetData = new FocusedTarget($data);
+                                $targetData->save();
+                            }
                         }
                     }
                 }
