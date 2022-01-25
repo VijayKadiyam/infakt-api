5d2a3215f4d2fb46f63985eacaf9bcc5346b9992
Khan-Sohail <ksohail.sk32@gmail.com>
2022-01-25 12:29:35 +0530

    OfftakeAnalytics Controller Modified: Added Supervisor Id Check in OfftakeAnalytics@noOrValueOfReports

--- app/Http/Controllers/OfftakeAnalyticsController.php
+++ app/Http/Controllers/OfftakeAnalyticsController.php
@@ -11,54 +11,58 @@ use App\Company;
 
 class OfftakeAnalyticsController extends Controller
 {
-	public function __construct() {
+	public function __construct()
+	{
 		$this->middleware(['auth:api', 'company'])
 			->except(['exports']);
 	}
 
 	public function masters(Request $request)
-  {
-    $months = [
-      ['text'  =>  'JANUARY', 'value' =>  1],
-      ['text'  =>  'FEBRUARY', 'value' =>  2],
-      ['text'  =>  'MARCH', 'value' =>  3],
-      ['text'  =>  'APRIL', 'value' =>  4],
-      ['text'  =>  'MAY', 'value' =>  5],
-      ['text'  =>  'JUNE', 'value' =>  6],
-      ['text'  =>  'JULY', 'value' =>  7],
-      ['text'  =>  'AUGUST', 'value' =>  8],
-      ['text'  =>  'SEPTEMBER', 'value' =>  9],
-      ['text'  =>  'OCTOBER', 'value' =>  10],
-      ['text'  =>  'NOVEMBER', 'value' =>  11],
-      ['text'  =>  'DECEMBER', 'value' =>  12],
-    ];
+	{
+		$months = [
+			['text'  =>  'JANUARY', 'value' =>  1],
+			['text'  =>  'FEBRUARY', 'value' =>  2],
+			['text'  =>  'MARCH', 'value' =>  3],
+			['text'  =>  'APRIL', 'value' =>  4],
+			['text'  =>  'MAY', 'value' =>  5],
+			['text'  =>  'JUNE', 'value' =>  6],
+			['text'  =>  'JULY', 'value' =>  7],
+			['text'  =>  'AUGUST', 'value' =>  8],
+			['text'  =>  'SEPTEMBER', 'value' =>  9],
+			['text'  =>  'OCTOBER', 'value' =>  10],
+			['text'  =>  'NOVEMBER', 'value' =>  11],
+			['text'  =>  'DECEMBER', 'value' =>  12],
+		];
 
-    $years = ['2020', '2021'];
+		$years = ['2020', '2021'];
 
-    return response()->json([
-      'months'  =>  $months,
-      'years'   =>  $years,
-    ], 200);
-  }
+		return response()->json([
+			'months'  =>  $months,
+			'years'   =>  $years,
+		], 200);
+	}
 
-	public function noOrValueOfReports(Request $request) 
+	public function noOrValueOfReports(Request $request)
 	{
 		$request->validate([
-			'month'  =>  'required',            
+			'month'  =>  'required',
 			'year'   =>  'required',
 			'type'	 =>		'required',
- 		]);
+		]);
 
 		$daysInMonth = 0;
 
-		$supervisors = 
+		$supervisors =
 			User::with('roles')
-				->where('active', '=', 1)
-				->whereHas('roles',  function ($q) {
-					$q->where('name', '=', 'SUPERVISOR');
-				})->orderBy('name')
-			// ->take(1)
-			->get();
+			->where('active', '=', 1)
+			->whereHas('roles',  function ($q) {
+				$q->where('name', '=', 'SUPERVISOR');
+			})->orderBy('name');
+		if ($request->superVisor_id) {
+			$supervisors = $supervisors->where('id', '=', $request->superVisor_id);
+		}
+		// ->take(1)
+		$supervisors = 	$supervisors->get();
 
 		$productsOfftakes = [];
 
@@ -81,7 +85,7 @@ class OfftakeAnalyticsController extends Controller
 					->with('order_details')
 					->whereHas('order_details',  function ($q) {
 						$q->groupBy('sku_id');
-					});	
+					});
 
 				if ($request->month) {
 					$ors = $ors->whereMonth('created_at', '=', $request->month);
@@ -93,26 +97,26 @@ class OfftakeAnalyticsController extends Controller
 
 				$daysInMonth = Carbon::parse($request->year . $request->month . '01')->daysInMonth;
 				$currentMonth = Carbon::now()->format('m');
-				if($request->month == $currentMonth) {
-					$daysInMonth = Carbon::now()->format('d');	
+				if ($request->month == $currentMonth) {
+					$daysInMonth = Carbon::now()->format('d');
 				}
-				for($i = 1; $i <= $daysInMonth; $i++) {
+				for ($i = 1; $i <= $daysInMonth; $i++) {
 					// To check single day orders
 					$ordersOfADay = [];
-					foreach($ors as $or) {
+					foreach ($ors as $or) {
 						// var_dump(Carbon::parse($or->created_at)->format('d'));
-						if(Carbon::parse($or->created_at)->format('d') == sprintf("%02d", $i)) {
+						if (Carbon::parse($or->created_at)->format('d') == sprintf("%02d", $i)) {
 							$ordersOfADay[] = $or;
 						}
 					}
 					// End To check single day orders
 
 					// If No of SKU per day is required
-					if($request->type == 'no') {
+					if ($request->type == 'no') {
 						$skuIDs = [];
-						foreach($ordersOfADay as $order) {
-							foreach($order->order_details as $order_detail) {
-								$skuIDs[] = $order_detail->sku_id; 
+						foreach ($ordersOfADay as $order) {
+							foreach ($order->order_details as $order_detail) {
+								$skuIDs[] = $order_detail->sku_id;
 							}
 						}
 						$uniqueSkuIDs = array_unique($skuIDs);
@@ -121,11 +125,11 @@ class OfftakeAnalyticsController extends Controller
 					}
 
 					// If value of SKUS required
-					if($request->type == 'value') {
+					if ($request->type == 'value') {
 						$totalValue = 0;
-						foreach($ordersOfADay as $order) {
-							foreach($order->order_details as $order_detail) {
-								$totalValue += $order_detail->value; 
+						foreach ($ordersOfADay as $order) {
+							foreach ($order->order_details as $order_detail) {
+								$totalValue += $order_detail->value;
 							}
 						}
 						$singleUserData['date' . $i] = $totalValue;
@@ -135,14 +139,15 @@ class OfftakeAnalyticsController extends Controller
 			}
 		}
 
-		return response()->json([   
+		return response()->json([
 			'count' =>  sizeof($productsOfftakes),
 			'data'  =>  $productsOfftakes,
 			'daysInMonth'	=>	$daysInMonth,
 		]);
 	}
 
-	public function exports(Request $request) {
+	public function exports(Request $request)
+	{
 		ini_set('max_execution_time', 10000);
 
 		$date = $request->date;
@@ -161,11 +166,11 @@ class OfftakeAnalyticsController extends Controller
 		$supervisors = User::with('roles')
 			->where('active', '=', 1)
 			->whereHas('roles',  function ($q) {
-			$q->where('name', '=', 'SUPERVISOR');
+				$q->where('name', '=', 'SUPERVISOR');
 			})->orderBy('name')
 			// ->take(1)
 			->get();
-			
+
 		foreach ($supervisors as $supervisor) {
 			$name = $supervisor->name;
 			Excel::store(new BAReportExport($date, $supervisor->id), "/reports/$date/$name-BAs-Report-$date.xlsx", 'local');
