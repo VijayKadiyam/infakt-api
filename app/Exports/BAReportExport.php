cea12d6b574cca63386e56857197cadc5900bac7
VijayKadiyam <freelancervijayk@gmail.com>
2022-02-04 10:57:32 +0530

    lc

--- app/Exports/BAReportExport.php
+++ app/Exports/BAReportExport.php
@@ -22,15 +22,15 @@ class BAReportExport implements WithMultipleSheets
     {
         $sheets = [];
         $sheets[] = new DailyAttendanceSheet($this->date, $this->supervisorId);
-        // $sheets[] = new MonthlyAttendanceSheet($this->date, $this->supervisorId);
-        // $sheets[] = new SkuOfftakesSheet($this->date, $this->supervisorId);
-        // $sheets[] = new SkuValueOfftakesSheet($this->date, $this->supervisorId);
-        // $sheets[] = new OfftakesSheet($this->date, $this->supervisorId);
-        // $sheets[] = new OfftakesCountSheet($this->date, $this->supervisorId);
-        // $sheets[] = new LeaveDefaulterSheet($this->date, $this->supervisorId);
-        // $sheets[] = new CustomerSheet($this->date, $this->supervisorId);
-        // $sheets[] = new CustomerDataEntrySheet($this->date, $this->supervisorId);
-        // $sheets[] = new CompetitorDataSheet($this->date, $this->supervisorId);
+        $sheets[] = new MonthlyAttendanceSheet($this->date, $this->supervisorId);
+        $sheets[] = new SkuOfftakesSheet($this->date, $this->supervisorId);
+        $sheets[] = new SkuValueOfftakesSheet($this->date, $this->supervisorId);
+        $sheets[] = new OfftakesSheet($this->date, $this->supervisorId);
+        $sheets[] = new OfftakesCountSheet($this->date, $this->supervisorId);
+        $sheets[] = new LeaveDefaulterSheet($this->date, $this->supervisorId);
+        $sheets[] = new CustomerSheet($this->date, $this->supervisorId);
+        $sheets[] = new CustomerDataEntrySheet($this->date, $this->supervisorId);
+        $sheets[] = new CompetitorDataSheet($this->date, $this->supervisorId);
 
         return $sheets;
     }
