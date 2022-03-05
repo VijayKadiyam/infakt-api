f7518238309f48f58b6f7c8e4cc96d5ed21e6fc1
VijayKadiyam <freelancervijayk@gmail.com>
2022-03-05 13:08:53 +0530

    lc

--- app/Http/Controllers/PjpSupervisorsController.php
+++ app/Http/Controllers/PjpSupervisorsController.php
@@ -83,6 +83,8 @@ class PjpSupervisorsController extends Controller
         ->get();
     }
 
+    $pjp_supervisors = $pjp_supervisors->jsonserialize();
+
     foreach ($pjp_supervisors as $key => $pjp) {
       $explodelocation = explode("#", $pjp_supervisors[$key]['pjp']['location']);
       $pjp_supervisors[$key]['pjp']['location'] = $explodelocation[0];
