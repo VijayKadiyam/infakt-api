275f8e99324014f3644dffb8034c4fcb4279b1f1
VijayKadiyam <freelancervijayk@gmail.com>
2022-03-08 08:02:04 +0530

    lc

--- app/Console/Commands/GenerateBaReportCommand.php
+++ app/Console/Commands/GenerateBaReportCommand.php
@@ -44,57 +44,57 @@ class GenerateBaReportCommand extends Command
         ini_set('max_execution_time', 0);
         ini_set('memory_limit', '-1');
 
-        // // $date = Carbon::now()->addDays(-1)->format('Y-m-d');
-        // $date = Carbon::now()->format('Y-m-d');
-        // $this->info('Generate Report for Date: ' . $date);
+        // $date = Carbon::now()->addDays(-1)->format('Y-m-d');
+        $date = Carbon::now()->format('Y-m-d');
+        $this->info('Generate Report for Date: ' . $date);
 
-        // // Excel::download(new BAReportExport($date), "BA-Report.xlsx");
+        // Excel::download(new BAReportExport($date), "BA-Report.xlsx");
 
-        // $supervisors = User::with('roles')
-        //     ->where('active', '=', 1)
-        //     ->whereHas('roles',  function ($q) {
-        //         $q->where('name', '=', 'SUPERVISOR');
-        //     })->orderBy('name')
-        //     // ->take(1) u
-        //     ->get();
+        $supervisors = User::with('roles')
+            ->where('active', '=', 1)
+            ->whereHas('roles',  function ($q) {
+                $q->where('name', '=', 'SUPERVISOR');
+            })->orderBy('name')
+            // ->take(1) u
+            ->get();
 
-        // $count = 1;
-        // foreach ($supervisors as $supervisor) {
-        //     $name = $supervisor->name;
-        //     Excel::store(new BAReportExport($date, $supervisor->id), "/reports/$date/$name-BAs-Report-$date.xlsx", 'local');
+        $count = 1;
+        foreach ($supervisors as $supervisor) {
+            $name = $supervisor->name;
+            Excel::store(new BAReportExport($date, $supervisor->id), "/reports/$date/$name-BAs-Report-$date.xlsx", 'local');
 
-        //     $this->info("$count. $name BAs Report Generated...");
-        //     $count++;
-        // }
+            $this->info("$count. $name BAs Report Generated...");
+            $count++;
+        }
 
         // Copmplete Report
         Excel::store(new BAReportExport($date), "/reports/$date/BA1-Report-$date.xlsx", "local");
 
         $this->info('BA Report Generated...');
 
-        // // Zone Code
-        // $regions = [
-        //     'North',
-        //     'South',
-        //     'East',
-        //     'West',
-        // ];
+        // Zone Code
+        $regions = [
+            'North',
+            'South',
+            'East',
+            'West',
+        ];
 
-        // foreach ($regions as $region) {
-        //     // return Excel::download(new BAReportExport($date,"",$region), "BA-Report-$date.xlsx");
-        //     Excel::store(new BAReportExport($date, '', $region), "/reports/$date/$region-BAs-Report-$date.xlsx", 'local');
-        // }
+        foreach ($regions as $region) {
+            // return Excel::download(new BAReportExport($date,"",$region), "BA-Report-$date.xlsx");
+            Excel::store(new BAReportExport($date, '', $region), "/reports/$date/$region-BAs-Report-$date.xlsx", 'local');
+        }
 
-        // // Channel Wise Report
-        // $channels = [
-        //     'IIA',
-        //     'GT',
-        //     'MT',
-        //     'ME_CNC',
-        // ];
+        // Channel Wise Report
+        $channels = [
+            'IIA',
+            'GT',
+            'MT',
+            'ME_CNC',
+        ];
 
-        // foreach ($channels as $key => $channel) {
-        //     return Excel::store(new BAReportExport($date, "", "", $channel), "$channel-BA-Report-$date.xlsx");
-        // }
+        foreach ($channels as $key => $channel) {
+            return Excel::store(new BAReportExport($date, "", "", $channel), "$channel-BA-Report-$date.xlsx");
+        }
     }
 }
