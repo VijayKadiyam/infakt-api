7cebb11dbbc9478e31de690a5641edf6a66c769f
Shaikh-Aabid <shaikhaabidhussain@gmail.com>
2022-03-14 17:20:19 +0530

    Add Mama Earth And Derma Export

--- app/Exports/BAReportExport.php
+++ app/Exports/BAReportExport.php
@@ -13,34 +13,36 @@ class BAReportExport implements WithMultipleSheets
     public $supervisorId;
     public $region;
     public $channel;
+    public $brand;
 
-    public function __construct($date, $supervisorId = '', $region = "", $channel = "")
+    public function __construct($date, $supervisorId = '', $region = "", $channel = "", $brand = "")
     {
         $this->date = $date;
         $this->supervisorId = $supervisorId;
         $this->region = $region;
         $this->channel = $channel;
+        $this->brand = $brand;
     }
 
     public function sheets(): array
     {
         $sheets = [];
-        $sheets[] = new DailyAttendanceSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new MonthlyAttendanceSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new SkuOfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new SkuValueOfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new OfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new OfftakesCountSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new LeaveDefaulterSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new CustomerSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new CustomerDataEntrySheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new CompetitorDataSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        // $sheets[] = new TargetSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        // $sheets[] = new FocusedTargetSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new ClosingStockSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new StockReportSheet($this->date, $this->supervisorId, $this->region, $this->channel);
+        $sheets[] = new DailyAttendanceSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new MonthlyAttendanceSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new SkuOfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new SkuValueOfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new OfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new OfftakesCountSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new LeaveDefaulterSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new CustomerSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new CustomerDataEntrySheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new CompetitorDataSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        // $sheets[] = new TargetSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        // $sheets[] = new FocusedTargetSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new ClosingStockSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
+        $sheets[] = new StockReportSheet($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
 
-        // $sheets[] = new ClosingStockSheet1($this->date, $this->supervisorId, $this->region, $this->channel);
+        $sheets[] = new ClosingStockSheet1($this->date, $this->supervisorId, $this->region, $this->channel, $this->brand);
         return $sheets;
     }
 }
