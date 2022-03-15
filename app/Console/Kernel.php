0db3a1e5084ad5b9a02981d9f852091658d98d8d
VijayKadiyam <freelancervijayk@gmail.com>
2022-03-15 07:17:47 +0530

    lc

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('generate:ba_report')->dailyAt('19:00');
         // $schedule->command('email:ba_report')->dailyAt('21:00');
 
-        $schedule->command('generate:ba_report')->dailyAt('19:41');
+        $schedule->command('generate:ba_report')->dailyAt('07:19');
         // $schedule->command('calculate:monthly_order_summary')->dailyAt('18:56');
         $schedule->command('email:ba_report')->dailyAt('09:00');
 
