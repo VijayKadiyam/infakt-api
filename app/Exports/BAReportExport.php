e8066e433b9649fa6c32d82cf9257250a0f113f9
VijayKadiyam <freelancervijayk@gmail.com>
2022-03-22 23:09:09 +0530

    lc

--- app/Exports/BAReportExport.php
+++ app/Exports/BAReportExport.php
@@ -27,28 +27,28 @@ class BAReportExport implements WithMultipleSheets
     public function sheets(): array
     {
         $sheets = [];
-$sheets[] = new DailyAttendanceSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new MonthlyAttendanceSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new SkuOfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new SkuValueOfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new OfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new OfftakesCountSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new LeaveDefaulterSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new CustomerSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new CustomerDataEntrySheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new CompetitorDataSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        // $sheets[] = new TargetSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        // $sheets[] = new FocusedTargetSheet($this->date, $this->supervisorId, $this->region, $this->channel);
+        $sheets[] = new DailyAttendanceSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new MonthlyAttendanceSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new SkuOfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new SkuValueOfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new OfftakesSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new OfftakesCountSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new LeaveDefaulterSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new CustomerSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new CustomerDataEntrySheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new CompetitorDataSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        // $sheets[] = new TargetSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        // $sheets[] = new FocusedTargetSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
         if ($this->supervisorId) {
-            $sheets[] = new ClosingStockSheet1($this->date, $this->supervisorId, $this->region, $this->channel);
+            $sheets[] = new ClosingStockSheet1($this->date, $this->supervisorId, $this->region, $this->channel, "");
         } else {
             $sheets[] = new ClosingStockSheet1($this->date, $this->supervisorId, $this->region, 'IIA');
             $sheets[] = new ClosingStockSheet1($this->date, $this->supervisorId, $this->region, 'GT');
             $sheets[] = new ClosingStockSheet1($this->date, $this->supervisorId, $this->region, 'MT');
             $sheets[] = new ClosingStockSheet1($this->date, $this->supervisorId, $this->region, 'MT - CNC');
         }
-        // $sheets[] = new ClosingStockSheet($this->date, $this->supervisorId, $this->region, $this->channel);
-        $sheets[] = new StockReportSheet($this->date, $this->supervisorId, $this->region, $this->channel);
+        // $sheets[] = new ClosingStockSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
+        $sheets[] = new StockReportSheet($this->date, $this->supervisorId, $this->region, $this->channel, "");
         return $sheets;
     }
 }
