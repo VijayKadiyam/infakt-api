a231b733dca366a2afb222b9b7f2efd0e8d0e172
VijayKadiyam <freelancervijayk@gmail.com>
2022-03-31 14:57:38 +0530

    lc

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends Controller
           'message' =>  "User is Logged in Successfully",
           'token'   =>  $user->api_token,
           'success' =>  true,
-          'currentAndroidVersionFromApi' =>  '1.0.33',
+          'currentAndroidVersionFromApi' =>  '1.0.34',
           'currentSupVersionFromApi' =>  '1.0.8',
       ]);
     }
