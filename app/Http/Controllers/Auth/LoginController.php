45cc3999c25bd252a258e5c4f916d798030519bc
VijayKadiyam <freelancervijayk@gmail.com>
2022-04-09 10:14:34 +0530

    ba api changed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends Controller
           'message' =>  "User is Logged in Successfully",
           'token'   =>  $user->api_token,
           'success' =>  true,
-          'currentAndroidVersionFromApi' =>  '1.0.34',
+          'currentAndroidVersionFromApi' =>  '1.0.35',
           'currentSupVersionFromApi' =>  '1.0.9',
       ]);
     }
