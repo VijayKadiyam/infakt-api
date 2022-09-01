12b2e3d727a18226022d4b0133c3f346780273ff
khan-Sohail <ksohail.sk32@gmail.com>
2022-09-01 13:33:55 +0530

    Content Metadata Module Modified ; * Added annotation field

--- database/factories/ContentMetadataFactory.php
+++ database/factories/ContentMetadataFactory.php
@@ -5,9 +5,10 @@ use Faker\Generator as Faker;
 
 $factory->define(ContentMetadata::class, function (Faker $faker) {
     return [
-        'content_id' => 1,
+        'content_id'    => 1,
         'metadata_type' => 'metadata_type',
-        'color_class' => 'color_class',
+        'color_class'   => 'color_class',
         'selected_text' => 'selected_text',
+        'annotation'    => 'annotation',
     ];
 });
