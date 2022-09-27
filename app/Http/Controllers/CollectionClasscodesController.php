214a094f198c1d61a499445db13627f3c41baecd
Shaikh-Aabid <shaikhaabidhussain@gmail.com>
2022-09-27 17:48:28 +0530

    Collection classcode modfed

--- app/Http/Controllers/CollectionClasscodesController.php
+++ app/Http/Controllers/CollectionClasscodesController.php
@@ -62,23 +62,23 @@ class CollectionClasscodesController extends Controller
 
         if (isset($request->collection_classcodes))
 
-            foreach ($request->collection_classcodes as $collecton) {
-                $collection_classcode = new CollectionClasscode($collecton);
+            foreach ($request->collection_classcodes as $collecton_classcode) {
+                $collection_classcode = new CollectionClasscode($collecton_classcode);
                 $strore = $request->company->collection_classcodes()->save($collection_classcode);
                 if ($strore) {
                     if ($collection_classcode->shared_by_id) {
-                        $user_classcodes = UserClasscode::where('classcode_id', $collection_classcode->classcode_id)->with('classcode')->get();
+                        $user_classcodes = UserClasscode::where('classcode_id', $collection_classcode->classcode_id)->with('user')->get();
                         foreach ($user_classcodes as $key => $uc) {
                             $description = "A new collection shared to you.";
-                            if (isset($uc->classcode->students)) {
-                                $user_id = $uc->user_id;
+                            if ($uc->user->roles[0]->name == 'STUDENT') {
+                                $user_id = $uc->user->id;
+                                $notification_data = [
+                                    'user_id' => $user_id,
+                                    'description' => $description
+                                ];
+                                $notifications = new Notification($notification_data);
+                                $request->company->notifications()->save($notifications);
                             }
-                            $notification_data = [
-                                'user_id' => $user_id,
-                                'description' => $description
-                            ];
-                            $notifications = new Notification($notification_data);
-                            $request->company->notifications()->save($notifications);
                         }
                     }
                 }
