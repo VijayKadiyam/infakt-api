44f22d6bdea81e2d264ee27b40fac62f7242cb05
VijayKadiyam <freelancervijayk@gmail.com>
2022-09-28 18:40:52 +0530

    lc

--- app/Http/Middleware/CompanyMiddleware.php
+++ app/Http/Middleware/CompanyMiddleware.php
@@ -4,6 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\Company;
+use App\UserTimestamp;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class CompanyMiddleware
 {
@@ -16,7 +19,16 @@ class CompanyMiddleware
    */
   public function handle($request, Closure $next)
   {
-    $request['company'] = Company::where('id' , '=', request()->header('company-id'))->first();
+    $userId = Auth::user()->id;
+    if ($userId && request()->header('company-id')) {
+      UserTimestamp::create([
+        'company_id' =>  request()->header('company-id'),
+        'user_id' =>  $userId,
+        "timestamp" =>  Carbon::now(),
+      ]);
+    }
+
+    $request['company'] = Company::where('id', '=', request()->header('company-id'))->first();
 
     return $next($request);
   }
--- app/UserTimestamp.php
+++ app/UserTimestamp.php
@@ -6,18 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserTimestamp extends Model
 {
-    protected $fillable =[
-        'user_id',
-        'timestamp',
-        'event',
-    ];
-    
-    public function company()
-    {
-      return $this->belongsTo(Company::class);
-    }
-    public function user()
-    {
-      return $this->belongsTo(User::class);
-    }
+  protected $fillable = [
+    'company_id',
+    'user_id',
+    'timestamp',
+    'event',
+  ];
+
+  public function company()
+  {
+    return $this->belongsTo(Company::class);
+  }
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
