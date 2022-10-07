2ba729a3f17830c38688217c166eb545bbe678e4
Nilamsurve26 <nilamsurve675@gmail.com>
2022-10-07 14:25:04 +0530

    Content_categories Done *TDD Done

---
+++ app/ContentCategory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContentCategory extends Model
+{
+    protected $fillable = [
+        'content_id',
+        'category_id',
+    ];
+    public function content()
+    {
+        return $this->belongsTo(Content::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(category::class);
+    }
+}
