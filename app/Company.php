76f8b4b9e212906ff8400ead2228f45fd606d10f
khan-Sohail <ksohail.sk32@gmail.com>
2022-10-07 20:07:21 +0530

    LC

--- app/Company.php
+++ app/Company.php
@@ -150,7 +150,8 @@ class Company extends Model
 
   public function notifications()
   {
-    return $this->hasMany(Notification::class);
+    return $this->hasMany(Notification::class)
+      ->where('is_deleted', false);
   }
 
   public function bookmark_classcodes()
@@ -234,5 +235,4 @@ class Company extends Model
   {
     return $this->hasMany(UserSubject::class);
   }
-
 }
