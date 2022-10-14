3cbcea2d12a92ab65ad441935c8207fedfe1c97d
VijayKadiyam <freelancervijayk@gmail.com>
2022-10-14 17:28:59 +0530

    Storage permission changed

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -3,7 +3,7 @@ cd /var/www/vhosts/infakt/infakt-api
 
 # php /usr/local/bin/composer install
 
-sudo chmod -R 755 storage public vendor bootstrap database
+sudo chmod -R 777 storage public vendor bootstrap database
 
 php /usr/local/bin/composer dump-autoload
 php artisan cache:clear
